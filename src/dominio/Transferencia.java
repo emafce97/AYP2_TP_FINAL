0b26d442e8b775284460b37c9635e7ff84970ddf
emafce97 <faillace45539@estudiantes.untref.edu.ar>
2025-07-26 20:31:23 -0300

    Se agrego un atributo de clase y de instancia a la clase transferencia.

--- src/dominio/Transferencia.java
+++ src/dominio/Transferencia.java
@@ -2,14 +2,18 @@ package dominio;
 
 public class Transferencia {
 
+    public static int ID;
+
     private String origen, destino, concepto;
     private double monto;
+    private int id;
 
     public Transferencia(String origen, String destino, String concepto, double monto) {
         this.origen = origen;
         this.destino = destino;
         this.concepto = concepto;
         this.monto = monto;
+        this.id = Transferencia.ID++;
     }
 
     public String getOrigen() {
@@ -42,6 +46,10 @@ public class Transferencia {
 
     public void setMonto(double monto) {
         this.monto = monto;
+    }
+
+    public int getId() {
+        return this.id;
     }
 
 }
